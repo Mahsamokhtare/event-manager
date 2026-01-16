7a1297c2cc66738e9b1e066a113037f2a9428898
mahsa.mokhtare <mahs.mokhtare@gmail.com>
2026-01-16 17:48:47 +0100

    header deleted on AppLayout

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router";
-import { Header } from "./Header";
 
 export const PublicLayout = () => {
   return (
     <>
-      <Header />
-
       <main>
         <Outlet />
       </main>
